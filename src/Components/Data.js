c0ffe8de1bdcaf37903ce2796c2ba6a69b718460
nsarojini <nsarojini.06@gmail.com>
2023-04-27 19:40:07 +0530

    first commit

---
+++ src/Components/Data.js
@@ -0,0 +1,39 @@
+import sunset1 from '../Images/sunset1.jpg'
+
+import parachute from '../Images/parachute.jpg'
+
+import roadscene from '../Images/roadscene.jpg'
+
+import lakescene from '../Images/lakescene.jpg'
+
+import mountains from '../Images/mountains.jpg'
+
+const Data = [
+    {
+        src : sunset1,
+        quotes : "When sunlight passes through more atmosphere, there are more particles to scatter the light. When the sky is the desirable bright red we so love in sunsets, it's an indication the atmosphere is full of moisture and dust particles. We see the red color because it has the longest wavelength in the spectrum"
+        
+    },
+    {
+        src : parachute,
+        quotes: "The sky is honestly the limit, so I'm excited to see what the future holds."
+        
+    },
+    {
+        src : roadscene,
+        quotes : "May your trails be crooked, winding, lonesome, dangerous, leading to the most amazing view. May your mountains rise into and above the clouds. May your rivers flow without end… ."
+        
+    },
+    {
+        src : lakescene,
+        quotes : "The tree which moves some to tears of joy is in the eyes of others only a green thing which stands in the way. Some see nature all ridicule and deformity, and by these I shall not regulate my propositions. And some see no nature at all. But to the eyes of the man of imagination, nature is imagination itself."
+        
+    },
+    {
+        src : mountains,
+        quotes : "There is something infinitely healing in the repeated refrains of nature — the assurance that dawn comes after night, and spring after the winter."
+        
+    }
+]
+
+export default Data
